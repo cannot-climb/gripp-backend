617b6e28968c495c4cf1ab395bb2a846e4479cb9
Na Jongwoo <njw1204@naver.com>
2022-09-25 14:14:19 +0900

    docs: add access token security scheme to OAS

--- gripp/src/main/java/kr/njw/gripp/global/config/OpenApiConfig.java
+++ gripp/src/main/java/kr/njw/gripp/global/config/OpenApiConfig.java
@@ -1,9 +1,17 @@
 package kr.njw.gripp.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "accessToken",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @OpenAPIDefinition(
         info = @Info(
                 title = "Gripp Api",

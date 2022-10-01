b548b1f03113122145fd49075502d7092c668bea
Na Jongwoo <njw1204@naver.com>
2022-10-01 12:07:34 +0900

    docs: fix leaderboard api OAS

--- gripp/src/main/java/kr/njw/gripp/user/controller/dto/FindLeaderBoardResponse.java
+++ gripp/src/main/java/kr/njw/gripp/user/controller/dto/FindLeaderBoardResponse.java
@@ -1,5 +1,6 @@
 package kr.njw.gripp.user.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -8,8 +9,14 @@ import java.util.List;
 
 @Data
 public class FindLeaderBoardResponse {
-    @Schema(description = "상위 리더보드 (전체 유저 상위 최대 10명)", maxLength = 10)
+    @ArraySchema(
+            arraySchema = @Schema(description = "상위 리더보드 (전체 유저 상위 최대 10명)"),
+            schema = @Schema(implementation = FindUserResponse.class),
+            maxItems = 10)
     private List<FindUserResponse> topBoard = new ArrayList<>();
-    @Schema(description = "내 근처 리더보드 (나를 포함하여 나보다 상위 최대 10명부터 하위 최대 10명까지, 총합 최대 21명)", maxLength = 21)
+    @ArraySchema(
+            arraySchema = @Schema(description = "내 근처 리더보드 (나를 포함하여 나보다 상위 최대 10명부터 하위 최대 10명까지, 총합 최대 21명)"),
+            schema = @Schema(implementation = FindUserResponse.class),
+            maxItems = 21)
     private List<FindUserResponse> defaultBoard = new ArrayList<>();
 }

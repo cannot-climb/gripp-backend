c598d9adc296e6c7fd3db1129e2ae1f4972d9987
Na Jongwoo <njw1204@naver.com>
2022-10-01 23:13:34 +0900

    docs: improve sign up api request description

--- gripp/src/main/java/kr/njw/gripp/auth/controller/dto/SignUpRequest.java
+++ gripp/src/main/java/kr/njw/gripp/auth/controller/dto/SignUpRequest.java
@@ -9,13 +9,16 @@ import javax.validation.constraints.Size;
 
 @Data
 public class SignUpRequest {
-    @Schema(description = "유저 아이디 (영숫자만 사용, 2자 이상 15자 이하)", example = "njw1204")
+    @Schema(description = "유저 아이디 (영문자와 숫자만 사용, 2자 이상 15자 이하)", example = "njw1204",
+            pattern = "^\\w{2,15}$")
     @NotEmpty(message = "must not be empty")
     @Size(min = 2, max = 15, message = "size must be between 2 and 15")
     @Pattern(regexp = "\\w*", message = "must contain only alphanumeric characters")
+    @Pattern(regexp = ".*") // springdoc hack
     private String username;
 
-    @Schema(description = "유저 비밀번호 (영숫자 모두 포함, 8자 이상 64자 이하)", example = "pass1234")
+    @Schema(description = "유저 비밀번호 (영문자 포함, 숫자 포함, 8자 이상 64자 이하)", example = "pass1234",
+            pattern = "^[\\w!\"#$%&'()*+,\\-.\\/:;<=>?@[\\]^_`{|}~\\\\]{8,64}$")
     @NotEmpty(message = "must not be empty")
     @Size(min = 8, max = 64, message = "size must be between 8 and 64")
     @Pattern(regexp = "[\\w\\p{Punct}]*", message = "must contain only alphanumeric characters and punctuations")

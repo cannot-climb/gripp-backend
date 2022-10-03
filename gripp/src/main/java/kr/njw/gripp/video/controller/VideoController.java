10c42cf1ad17cdceb05bf7e6b5c36954d0294c70
Na Jongwoo <njw1204@naver.com>
2022-10-03 16:44:42 +0900

    docs: add upload video api operation description

--- gripp/src/main/java/kr/njw/gripp/video/controller/VideoController.java
+++ gripp/src/main/java/kr/njw/gripp/video/controller/VideoController.java
@@ -32,7 +32,21 @@ import java.util.List;
 public class VideoController {
     private final VideoApplication videoApplication;
 
-    @Operation(summary = "영상 업로드", description = "영상 업로드 API")
+    @Operation(summary = "영상 업로드", description = """
+            영상 업로드 API
+
+            multipart/form-data 형식으로 업로드
+
+            게시물 등록 전에 단순히 파일만 업로드하는 API임\040\040
+            영상 처리 및 게시물 등록은 나중에 이루어짐\040\040
+            업로드가 완료되면 영상 아이디를 받고 그 값을 게시물 등록 API에 보내면 됨
+
+            파일 업로드가 끝나도 완료 시점에 엑세스 토큰이 만료된 상태이면 HTTP 401 에러가 반환됨\040\040
+            따라서 업로드가 길어지면 업로드 중간에 토큰이 만료되어 HTTP 401 에러가 반환될 수 있음\040\040
+            업로드 중 토큰 만료 방지를 위해, 반드시 토큰 갱신 API를 먼저 한 번 호출하고 업로드 진행 바람
+
+            파일 업로드는 다소 시간이 걸림 (유선 네트워크에서 100MB 업로드에 빨라야 10초)\040\040
+            실패하는 경우도 많을 수 있으니 예외 처리 바람""")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "업로드 완료",
                     content = @Content(schema = @Schema(implementation = UploadVideoResponse.class))),

dd2268160b33e20e62586ac8ca267fe056624ac8
Na Jongwoo <njw1204@naver.com>
2022-10-04 01:19:11 +0900

    feat: download with file system resource to preserve content-length

--- gripp/src/main/java/kr/njw/gripp/download/controller/DownloadController.java
+++ gripp/src/main/java/kr/njw/gripp/download/controller/DownloadController.java
@@ -10,26 +10,23 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.njw.gripp.global.config.GrippConfig;
 import kr.njw.gripp.global.dto.ErrorResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Tag(name = "Download")
 @SecurityRequirement(name = "admin")
@@ -54,22 +51,22 @@ public class DownloadController {
     })
     @GetMapping(value = "/{fileName}")
     public ResponseEntity<?> download(@PathVariable("fileName") String fileName) {
-        try {
-            Path path = Paths.get(GrippConfig.FILE_UPLOAD_PATH, fileName);
-            Resource resource = new InputStreamResource(Files.newInputStream(path));
-            HttpHeaders headers = new HttpHeaders();
+        Path path = Paths.get(GrippConfig.FILE_UPLOAD_PATH, FilenameUtils.getName(fileName));
+        Resource resource = new FileSystemResource(path);
+        HttpHeaders headers = new HttpHeaders();
 
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            headers.set("Content-Disposition",
-                    "attachment; filename*=UTF-8''" +
-                            URLEncoder.encode(fileName, StandardCharsets.UTF_8).replace("+", "%20"));
-            headers.set("Cache-Control", "max-age=86400");
-            return ResponseEntity.ok().headers(headers).body(resource);
-        } catch (IOException e) {
-            this.logger.warn("파일이 없습니다 - " + e);
+        if (!resource.exists()) {
+            this.logger.warn("파일이 없습니다 - " + fileName);
             ErrorResponse errorResponse = new ErrorResponse();
             errorResponse.setErrors(List.of("unknown file"));
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
+
+        headers.setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS));
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.set("Content-Disposition",
+                "attachment; filename*=UTF-8''" +
+                        URLEncoder.encode(fileName, StandardCharsets.UTF_8).replace("+", "%20"));
+        return ResponseEntity.ok().headers(headers).body(resource);
     }
 }

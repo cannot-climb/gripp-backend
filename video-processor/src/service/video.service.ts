78ac22f82cbb4d6cb73ea7e1d4cabf82e5dcec4c
Na Jongwoo <njw1204@naver.com>
2022-10-07 22:58:07 +0900

    fix: ffmpeg too slow to download for mov

--- video-processor/src/service/video.service.ts
+++ video-processor/src/service/video.service.ts
@@ -12,16 +12,17 @@ export class VideoService {
     start: number,
     end: number,
   ) {
-    const basicToken = Buffer.from(
-      `${process.env.GRIPP_ADMIN_NAME}:${process.env.GRIPP_ADMIN_PASSWORD}`,
-    ).toString('base64');
     const thumbnailSS = Math.trunc((end - start) / 2);
-    const command = `ffmpeg -hide_banner -nostdin -y \\
-      -headers $'Authorization: Basic ${basicToken}\\r\\n' \\
+    const wgetCommand = `wget --no-verbose \\
+      --user=${process.env.GRIPP_ADMIN_NAME} \\
+      --password=${process.env.GRIPP_ADMIN_PASSWORD} \\
+      -O videos/${fileName} \\
+      ${this.GRIPP_DOWNLOAD_API}/${fileName}`;
+    const ffmpegCommand = `ffmpeg -hide_banner -nostdin -y -loglevel warning \\
       -ss ${start} -to ${end} \\
-      -i ${this.GRIPP_DOWNLOAD_API}/${fileName} \\
+      -i videos/${fileName} \\
       -filter_complex "[v]split=2[vt1][vt2];[vt1]scale=720:-2:flags=lanczos,format=yuv420p[vo1];[vt2]scale=480:-2:flags=lanczos,format=yuv420p[vo2]" \\
-      -preset fast -crf 20 -sc_threshold 0 \\
+      -preset veryfast -crf 20 -sc_threshold 0 \\
       -force_key_frames "expr:gte(t,n_forced*2)" \\
       -map [vo1] -codec:v:0 h264 -profile:v:0 main -b:v:0 3000K -maxrate:v:0 3500K -bufsize:v:0 4500K \\
       -map [vo2] -codec:v:1 h264 -profile:v:1 main -b:v:1 1500K -maxrate:v:1 1750K -bufsize:v:1 2250K \\
@@ -40,10 +41,24 @@ export class VideoService {
       fs.mkdirSync(`videos/${uuid}`, { recursive: true });
     }
 
-    const ffmpeg = execa.command(command, opt);
-    ffmpeg.stdout?.pipe(process.stdout);
-    ffmpeg.stderr?.pipe(process.stderr);
+    try {
+      const wget = execa.command(wgetCommand, opt);
+      wget.stdout?.pipe(process.stdout);
+      wget.stderr?.pipe(process.stderr);
 
-    await ffmpeg;
+      await wget;
+
+      const ffmpeg = execa.command(ffmpegCommand, opt);
+      ffmpeg.stdout?.pipe(process.stdout);
+      ffmpeg.stderr?.pipe(process.stderr);
+
+      await ffmpeg;
+    } finally {
+      fs.unlink(`videos/${fileName}`, (err) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
   }
 }

8297bc5179605b55ea030e1a2567f7090685cad1
Na Jongwoo <njw1204@naver.com>
2022-10-11 19:08:04 +0900

    feat: improve video-processor ffmpeg performance

--- video-processor/src/service/video.service.ts
+++ video-processor/src/service/video.service.ts
@@ -12,16 +12,28 @@ export class VideoService {
     start: number,
     end: number,
   ) {
-    const thumbnailSS = start;
-    const wgetCommand = `wget --no-verbose \\
+    const opt = { shell: 'bash' };
+
+    if (!fs.existsSync(`videos/${uuid}`)) {
+      fs.mkdirSync(`videos/${uuid}`, { recursive: true });
+    }
+
+    try {
+      const wgetCommand = `wget --no-verbose \\
       --user=${process.env.GRIPP_ADMIN_NAME} \\
       --password=${process.env.GRIPP_ADMIN_PASSWORD_RAW} \\
       -O videos/${fileName} \\
       ${this.GRIPP_DOWNLOAD_API}/${fileName}`;
-    const ffmpegCommand = `ffmpeg -hide_banner -nostdin -y -loglevel warning \\
+
+      const wget = execa.command(wgetCommand, opt);
+      wget.stdout?.pipe(process.stdout);
+      wget.stderr?.pipe(process.stderr);
+      await wget;
+
+      const hlsCommand = `ffmpeg -hide_banner -nostdin -y \\
       -ss ${start} -to ${end} \\
       -i videos/${fileName} \\
-      -filter_complex "[v]split=2[vt1][vt2];[vt1]scale=720:-2:flags=lanczos,format=yuv420p[vo1];[vt2]scale=480:-2:flags=lanczos,format=yuv420p[vo2]" \\
+      -filter_complex "[v]split=2[vt1][vt2];[vt1]scale=720:-2,format=yuv420p[vo1];[vt2]scale=480:-2,format=yuv420p[vo2]" \\
       -preset veryfast -crf 20 -sc_threshold 0 \\
       -force_key_frames "expr:gte(t,n_forced*2)" \\
       -map [vo1] -codec:v:0 h264 -profile:v:0 main -b:v:0 3000K -maxrate:v:0 3500K -bufsize:v:0 4500K \\
@@ -32,33 +44,36 @@ export class VideoService {
       -f hls -hls_time 6 -hls_list_size 0 -hls_segment_type mpegts -hls_playlist_type vod \\
       -master_pl_name master.m3u8 \\
       -hls_segment_filename videos/${uuid}/stream_%v_%03d.ts \\
-      videos/${uuid}/stream_%v.m3u8 \\
-      -ss ${thumbnailSS} -frames:v 1 -filter:v "scale=480:-2:flags=lanczos" \\
+      videos/${uuid}/stream_%v.m3u8`;
+
+      const hls = execa.command(hlsCommand, opt);
+      hls.stdout?.pipe(process.stdout);
+      hls.stderr?.pipe(process.stderr);
+      await hls;
+
+      const ffprobeCommand = `ffprobe -v quiet -of json \\
+      -select_streams v \\
+      -show_format -show_streams \\
+      videos/${uuid}/stream_720.m3u8`;
+
+      const ffprobe = execa.command(ffprobeCommand, opt);
+      ffprobe.stdout?.pipe(process.stdout);
+      ffprobe.stderr?.pipe(process.stderr);
+      const ffprobeResult = JSON.parse((await ffprobe).stdout || '{}');
+
+      const thumbnailCommand = `ffmpeg -hide_banner -nostdin -y \\
+      -ss ${(Number(ffprobeResult?.format?.duration || 0) / 2).toFixed(6)} \\
+      -i videos/${uuid}/stream_480.m3u8 \\
+      -frames:v 1 \\
+      -f image2 -update 1 \\
       videos/${uuid}/thumbnail.png`;
-    const opt = { shell: 'bash' };
 
-    if (!fs.existsSync(`videos/${uuid}`)) {
-      fs.mkdirSync(`videos/${uuid}`, { recursive: true });
-    }
+      const thumbnail = execa.command(thumbnailCommand, opt);
+      thumbnail.stdout?.pipe(process.stdout);
+      thumbnail.stderr?.pipe(process.stderr);
+      await thumbnail;
 
-    try {
-      console.log(wgetCommand);
-
-      const wget = execa.command(wgetCommand, opt);
-      wget.stdout?.pipe(process.stdout);
-      wget.stderr?.pipe(process.stderr);
-
-      await wget;
-
-      console.log(ffmpegCommand);
-
-      const ffmpeg = execa.command(ffmpegCommand, opt);
-      ffmpeg.stdout?.pipe(process.stdout);
-      ffmpeg.stderr?.pipe(process.stderr);
-
-      await ffmpeg;
-
-      console.log(`영상 인코딩 완료 - videos/${uuid}`);
+      console.log(`영상 인코딩 완료 - ${uuid}/master.m3u8`);
     } finally {
       fs.unlink(`videos/${fileName}`, (err) => {
         if (err) {

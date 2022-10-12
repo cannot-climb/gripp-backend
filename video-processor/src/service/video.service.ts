f3d5a5b405f39c862e57d5342d7af83003579ecf
Na Jongwoo <njw1204@naver.com>
2022-10-12 14:29:29 +0900

    refactor: constant video encoding options

--- video-processor/src/service/video.service.ts
+++ video-processor/src/service/video.service.ts
@@ -8,7 +8,13 @@ import path from 'path';
 
 @Injectable()
 export class VideoService {
+  private readonly HIGH_RES = 720;
+  private readonly MEDIUM_RES = 480;
+  private readonly HLS_MASTER_FILE_NAME = 'master.m3u8';
+  private readonly HLS_THUMBNAIL_FILE_NAME = 'thumbnail.png';
   private readonly GRIPP_DOWNLOAD_API = 'https://gripp.dev.njw.kr/download';
+  private readonly GRIPP_DEEP_API =
+    'http://gripp-deep.njw.kr/kilterboard/upload';
   private readonly s3 = new AWS.S3({
     credentials: {
       accessKeyId: `${process.env.GRIPP_AWS_ACCESS_KEY}`,
@@ -40,7 +46,7 @@ export class VideoService {
       await wget;
 
       const deepNetworkResponse = await axios.post(
-        'http://gripp-deep.njw.kr/kilterboard/upload',
+        this.GRIPP_DEEP_API,
         {
           videoUrl: `${this.GRIPP_DOWNLOAD_API}/${fileName}`,
         },
@@ -73,16 +79,16 @@ export class VideoService {
       const hlsCommand = `ffmpeg -hide_banner -nostdin -y \\
       -ss ${startTime} -to ${endTime} \\
       -i videos/${fileName} \\
-      -filter_complex "[v]split=2[vt1][vt2];[vt1]scale=720:-2,format=yuv420p[vo1];[vt2]scale=480:-2,format=yuv420p[vo2]" \\
+      -filter_complex "[v]split=2[vt1][vt2];[vt1]scale=${this.HIGH_RES}:-2,format=yuv420p[vo1];[vt2]scale=${this.MEDIUM_RES}:-2,format=yuv420p[vo2]" \\
       -preset veryfast -crf 20 -sc_threshold 0 \\
       -force_key_frames "expr:gte(t,n_forced*2)" \\
       -map [vo1] -codec:v:0 h264 -profile:v:0 main -b:v:0 3000K -maxrate:v:0 3500K -bufsize:v:0 4500K \\
       -map [vo2] -codec:v:1 h264 -profile:v:1 main -b:v:1 1500K -maxrate:v:1 1750K -bufsize:v:1 2250K \\
       -map a -codec:a:0 aac -b:a:0 128K \\
       -map a -codec:a:1 aac -b:a:1 128K \\
-      -var_stream_map "v:0,a:0,name:720 v:1,a:1,name:480" \\
+      -var_stream_map "v:0,a:0,name:${this.HIGH_RES} v:1,a:1,name:${this.MEDIUM_RES}" \\
       -f hls -hls_time 6 -hls_list_size 0 -hls_segment_type mpegts -hls_playlist_type vod \\
-      -master_pl_name master.m3u8 \\
+      -master_pl_name ${this.HLS_MASTER_FILE_NAME} \\
       -hls_segment_filename videos/${uuid}/stream_%v_%03d.ts \\
       videos/${uuid}/stream_%v.m3u8`;
       console.log(hlsCommand);
@@ -95,7 +101,7 @@ export class VideoService {
       const ffprobeCommand = `ffprobe -v quiet -of json \\
       -select_streams v \\
       -show_format -show_streams \\
-      videos/${uuid}/stream_720.m3u8`;
+      videos/${uuid}/stream_${this.HIGH_RES}.m3u8`;
 
       const ffprobe = execa.command(ffprobeCommand, opt);
       ffprobe.stdout?.pipe(process.stdout);
@@ -104,10 +110,10 @@ export class VideoService {
 
       const thumbnailCommand = `ffmpeg -hide_banner -nostdin -y \\
       -ss ${(Number(ffprobeResult?.format?.duration || 0) / 2).toFixed(6)} \\
-      -i videos/${uuid}/stream_480.m3u8 \\
+      -i videos/${uuid}/stream_${this.MEDIUM_RES}.m3u8 \\
       -frames:v 1 \\
       -f image2 -update 1 \\
-      videos/${uuid}/thumbnail.png`;
+      videos/${uuid}/${this.HLS_THUMBNAIL_FILE_NAME}`;
       console.log(thumbnailCommand);
 
       const thumbnail = execa.command(thumbnailCommand, opt);
